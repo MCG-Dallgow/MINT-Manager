56518706aa81e899d1975e288f3ee471998144e4
SLH335 <114884726+SLH335@users.noreply.github.com>
2023-07-18 21:39:36 +0200

    Set application name and version

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 
 #include <QApplication>
 
+#define APPLICATION_NAME "MINT-Verwaltung"
+#define APPLICATION_VERSION "0.1.0"
+
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setApplicationName(APPLICATION_NAME);
+    QCoreApplication::setApplicationVersion(APPLICATION_VERSION);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
